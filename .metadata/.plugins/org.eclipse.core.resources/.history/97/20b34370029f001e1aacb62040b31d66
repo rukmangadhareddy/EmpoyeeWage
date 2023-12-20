d2478b00c7cfb644d705281ede72006621171ed9
IQBAL DG <rukmangadhareddy43@gmail.com>
2023-12-20 12:22:11 +0530

    Master Branch created

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/97/20b34370029f001e1aacb62040b31d66
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeewage;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ EmployeeWage/src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.employeewage;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		
+		
+System.out.println("Welcome to Emloyee Wage Computation program");
+	}
+
+}
