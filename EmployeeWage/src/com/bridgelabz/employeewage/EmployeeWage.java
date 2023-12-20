391d6111c7a9fc475decfc3def05c6cc172cdc66
IQBAL DG <rukmangadhareddy43@gmail.com>
2023-12-20 12:50:47 +0530

    added Uc1

--- EmployeeWage/src/com/bridgelabz/employeewage/EmployeeWage.java
+++ EmployeeWage/src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,11 +1,15 @@
 package com.bridgelabz.employeewage;
-
+import java.util.Random;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		
+		boolean present = true;
 		
-System.out.println("Welcome to Emloyee Wage Computation program");
+		Random random = new Random();
+		if (present == random.nextBoolean()) {
+			System.out.println("Employee is Present");
+		}else System.out.println("Employee is Absent");
 	}
 
 }
